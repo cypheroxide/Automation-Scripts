e9941db77298f04dec29df5f9f04db5e954085e8
Angel Santiago <72179176+cypheroxide@users.noreply.github.com>
2023-10-10 18:10:26 -0500

    Update updt.sh
    Removed superfluous flags

--- updt.sh
+++ updt.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
+#
 #This is a simple bash script to check for updates to the system and will begin
 #the installation process automatically.
-
-
-sudo apt update -yy
-sudo apt full-upgrade -yy
-sudo apt dist-upgrade -yy
-sudo apt autoremove -yy
+#
+#
+#
+sudo apt update -y
+sudo apt full-upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
 sudo apt purge
 sudo apt clean
-sudo apt update -yy
-sudo apt full-upgrade -yy
-exit
-
+sudo apt update -y
+sudo apt full-upgrade -y
+#
+#
+#
+#
+# end of script

35e9e62fb19dc9990533ee11e9be08ddd3225bc7
Angel Santiago <72179176+cypheroxide@users.noreply.github.com>
2023-10-10 18:11:25 -0500

    Update updt.sh
    Remove flags on 'update' commands

--- updt.sh
+++ updt.sh
@@ -5,13 +5,13 @@
 #
 #
 #
-sudo apt update -y
+sudo apt update
 sudo apt full-upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt purge
 sudo apt clean
-sudo apt update -y
+sudo apt update
 sudo apt full-upgrade -y
 #
 #
